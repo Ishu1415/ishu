3e4497afc97fedac469a9e54bf0a4c854e678aca
Ishu1415 <ishurulhan@gmail.com>
2025-06-27 21:35:35 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,38 @@
+// console.log(Math.abs(5));
+// console.log(Math.abs(Infinity));
+// console.log(Math.abs("hello"));
+// console.log(Math.abs(""));
+
+// console.log(Math.max(-4,2));
+// console.log(Math.max(32,21));
+// console.log(Math.min(4,2));
+// console.log(Math.min(-9,0.67));
+
+// console.log(Math.sin(60));
+// console.log(Math.sin(0));
+// console.log(Math.sin(45));
+
+// console.log(Math.cos(0));
+// console.log(Math.cos(45));
+
+// console.log(Math.tan(45));
+// console.log(Math.tan(30));
+
+// console.log(Math.round(4.7));
+// console.log(Math.round(8.5));
+
+// console.log(Math.floor(4.9));
+// console.log(Math.floor(6.2));
+// console.log(Math.floor(9.5));
+
+// console.log(Math.ceil(8.3));
+// console.log(Math.ceil(89.20));
+// console.log(Math.ceil(23.9));
+
+// console.log(Math.pow(2,3));
+// console.log(Math.pow(25,2));
+
+//console.log(Math.random());
+
+// console.log(Math.log10(2));
+// console.log(Math.log2(45));
