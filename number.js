6fe65de848e6548eaa985d5bdf9a180a1502477c
Ishu1415 <ishurulhan@gmail.com>
2025-06-27 18:41:53 +0530

    Add files via upload

---
+++ number.js
@@ -0,0 +1,38 @@
+let num=3474;
+//let num2=.5;
+//console.log(Number.isFinite(num,num2));
+
+//console.log(Number.isInteger(5.0009));
+
+//console.log(Number.isSafeInteger(5));
+
+// let value="23.56";
+// console.log(Number.parseFloat(value));
+
+// let intValue="38974";
+// console.log(Number.parseInt(intValue));
+
+// console.log(Number.parseInt(value));
+
+// console.log(Number.parseFloat(intValue));
+
+// console.log(Number.POSITIVE_INFINITY);
+// console.log(Number.NEGATIVE_INFINITY);
+
+//let x=Number.MAX_SAFE_INTEGER+1;
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MAX_VALUE);
+// console.log(x);
+
+// let x=Number.MIN_SAFE_INTEGER-1;
+// console.log(Number.MIN_SAFE_INTEGER);
+// console.log(Number.MIN_VALUE);
+// console.log(x);
+
+//console.log((6757646).toLocaleString());
+
+console.log((89473298).toString());
+
+console.log(typeof num);
+
+console.log((0.4).toFixed(40));
